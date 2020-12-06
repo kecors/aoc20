dea8580f7015986da818fad316de8241c8af421e
Kenneth Collins <dev@kecors.net>
2020-12-05 19:44:15 -0500

    Solve day 1, part 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,32 @@
+use std::collections::HashSet;
+use std::io::{stdin, Read};
+
+fn solve(items: &Vec<u32>) -> u32 {
+    let mut values: HashSet<u32> = HashSet::new();
+
+    for &item in items.iter() {
+        let complement: u32 = 2020 - item;
+
+        if values.contains(&complement) {
+            return item * complement;
+        }
+
+        values.insert(item);
+    }
+
+    panic!("No solution available");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+
+    let items: Vec<u32> = input
+        .lines()
+        .map(|x| x.trim().parse::<u32>().unwrap())
+        .collect();
+
+    let x = solve(&items);
+
+    println!("Part 1: the product is {}", &x);
 }
