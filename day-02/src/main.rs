dc24221cd879ee1d984bdc0586d9faf81c24dba6
Kenneth Collins <dev@kecors.net>
2020-12-08 18:35:08 -0500

    Solve day 2, part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -2,12 +2,7 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::io::{stdin, Read};
 
-fn main() {
-    let mut input = String::new();
-    stdin().read_to_string(&mut input).unwrap();
-
-    let items: Vec<&str> = input.lines().collect();
-
+fn solve_part_1(items: &[&str]) -> u32 {
     let rx = Regex::new(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$").unwrap();
     let mut counter: u32 = 0;
 
@@ -33,5 +28,39 @@ fn main() {
         }
     }
 
-    println!("Part 1: {} passwords are valid", counter);
+    counter
+}
+
+fn solve_part_2(items: &[&str]) -> u32 {
+    let rx = Regex::new(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$").unwrap();
+    let mut counter: u32 = 0;
+
+    for item in items.iter() {
+        if let Some(cap) = rx.captures(item) {
+            let position_1: usize = cap[1].parse::<usize>().unwrap() - 1;
+            let position_2: usize = cap[2].parse::<usize>().unwrap() - 1;
+            let letter: char = cap[3].chars().next().unwrap();
+            let password: Vec<char> = cap[4].chars().collect();
+
+            let state_1 = letter == password[position_1];
+            let state_2 = letter == password[position_2];
+            if state_1 ^ state_2 {
+                counter += 1;
+            }
+        } else {
+            println!("Invalid input: {}", item);
+        }
+    }
+
+    counter
+}
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+
+    let items: Vec<&str> = input.lines().collect();
+
+    println!("Part 1: {} passwords are valid", solve_part_1(&items));
+    println!("Part 2: {} passwords are valid", solve_part_2(&items));
 }
