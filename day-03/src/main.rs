4efd8ff2f76e3a5c1b44522c2b32163b219bcc36
Kenneth Collins <dev@kecors.net>
2020-12-08 19:26:01 -0500

    Solve day 3, part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -6,6 +6,22 @@ enum Square {
     Open,
 }
 
+fn calculate_encounters(area: &[Vec<Square>], x_offset: usize, y_offset: usize) -> u32 {
+    let mut x = 0;
+    let mut y = 0;
+    let mut tree_encounters = 0;
+
+    while y < area.len() {
+        if area[y][x % area[0].len()] == Square::Tree {
+            tree_encounters += 1;
+        }
+        x += x_offset;
+        y += y_offset;
+    }
+
+    tree_encounters
+}
+
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
@@ -19,17 +35,14 @@ fn main() {
         })
         .collect();
 
-    let mut x = 0;
-    let mut y = 0;
-    let mut tree_encounters = 0;
-
-    while y < area.len() {
-        if area[y][x % area[0].len()] == Square::Tree {
-            tree_encounters += 1;
-        }
-        x += 3;
-        y += 1;
-    }
-
-    println!("Part 1: You would encounter {} trees", tree_encounters);
+    println!(
+        "Part 1: You would encounter {} trees",
+        calculate_encounters(&area, 3, 1)
+    );
+    let tree_encounters = calculate_encounters(&area, 1, 1)
+        * calculate_encounters(&area, 3, 1)
+        * calculate_encounters(&area, 5, 1)
+        * calculate_encounters(&area, 7, 1)
+        * calculate_encounters(&area, 1, 2);
+    println!("Part 2: You would encounter {} trees", tree_encounters);
 }
