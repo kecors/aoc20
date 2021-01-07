df68f4933ddf48275306eeb7573f000e9e9e4100
Kenneth Collins <dev@kecors.net>
2021-01-07 09:25:06 -0500

    Solve day 17, part 2

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -10,16 +10,20 @@ struct Extent {
     max_y: i16,
     min_z: i16,
     max_z: i16,
+    min_w: i16,
+    max_w: i16,
 }
 
 impl Extent {
-    fn new(cubes: &HashSet<(i16, i16, i16)>) -> Extent {
+    fn new(cubes: &HashSet<(i16, i16, i16, i16)>) -> Extent {
         let mut min_x = 0;
         let mut max_x = 0;
         let mut min_y = 0;
         let mut max_y = 0;
         let mut min_z = 0;
         let mut max_z = 0;
+        let mut min_w = 0;
+        let mut max_w = 0;
 
         for cube in cubes.iter() {
             min_x = cmp::min(min_x, cube.0);
@@ -28,6 +32,8 @@ impl Extent {
             max_y = cmp::max(max_y, cube.1);
             min_z = cmp::min(min_z, cube.2);
             max_z = cmp::max(max_z, cube.2);
+            min_w = cmp::min(min_w, cube.3);
+            max_w = cmp::max(max_w, cube.3);
         }
 
         Extent {
@@ -37,13 +43,15 @@ impl Extent {
             max_y,
             min_z,
             max_z,
+            min_w,
+            max_w,
         }
     }
 }
 
 #[derive(Debug)]
 struct Engine {
-    actives: HashSet<(i16, i16, i16)>,
+    actives: HashSet<(i16, i16, i16, i16)>,
 }
 
 impl Engine {
@@ -55,31 +63,33 @@ impl Engine {
                 .enumerate()
                 .filter(|(_, c)| *c == '#')
                 .for_each(|(x, _)| {
-                    actives.insert((x as i16, y as i16, 0));
+                    actives.insert((x as i16, y as i16, 0, 0));
                 })
         });
 
         Engine { actives }
     }
 
-    fn count_active_neighbors(&self, cube: &(i16, i16, i16)) -> usize {
+    fn count_active_neighbors(&self, cube: &(i16, i16, i16, i16)) -> usize {
         let mut neighbors = HashSet::new();
 
         for x in cube.0 - 1..=cube.0 + 1 {
             for y in cube.1 - 1..=cube.1 + 1 {
                 for z in cube.2 - 1..=cube.2 + 1 {
-                    neighbors.insert((x, y, z));
+                    for w in cube.3 - 1..=cube.3 + 1 {
+                        neighbors.insert((x, y, z, w));
+                    }
                 }
             }
         }
 
         // A cube cannot be its own neighbor
-        neighbors.remove(&(cube.0, cube.1, cube.2));
+        neighbors.remove(&(cube.0, cube.1, cube.2, cube.3));
 
         neighbors.intersection(&self.actives).count()
     }
 
-    fn execute_cycle(&mut self) {
+    fn execute_cycle(&mut self, part: u8) {
         let mut new_actives = HashSet::new();
 
         let extent = Extent::new(&self.actives);
@@ -87,17 +97,22 @@ impl Engine {
         for x in extent.min_x - 1..=extent.max_x + 1 {
             for y in extent.min_y - 1..=extent.max_y + 1 {
                 for z in extent.min_z - 1..=extent.max_z + 1 {
-                    let cube = (x, y, z);
-                    match self.count_active_neighbors(&cube) {
-                        2 => {
-                            if self.actives.contains(&cube) {
+                    for w in extent.min_w - 1..=extent.max_w + 1 {
+                        if part == 1 && w != 0 {
+                            continue;
+                        }
+                        let cube = (x, y, z, w);
+                        match self.count_active_neighbors(&cube) {
+                            2 => {
+                                if self.actives.contains(&cube) {
+                                    new_actives.insert(cube);
+                                };
+                            }
+                            3 => {
                                 new_actives.insert(cube);
-                            };
+                            }
+                            _ => {}
                         }
-                        3 => {
-                            new_actives.insert(cube);
-                        }
-                        _ => {}
                     }
                 }
             }
@@ -112,7 +127,7 @@ impl Engine {
 
         for y in (extent.min_y..=extent.max_y).rev() {
             for x in extent.min_x..=extent.max_x {
-                let cube = (x, y, 0);
+                let cube = (x, y, 0, 0);
                 if self.actives.contains(&cube) {
                     print!("#");
                 } else {
@@ -129,14 +144,21 @@ fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let mut engine = Engine::new(&input);
-    //engine.display_cubes();
+    let mut engine_p1 = Engine::new(&input);
 
     for _ in 0..6 {
-        engine.execute_cycle();
-        //engine.display_cubes();
+        engine_p1.execute_cycle(1);
     }
 
-    let count = engine.actives.iter().count();
+    let count = engine_p1.actives.iter().count();
     println!("Part 1: {} cubes are active after six cycles", count);
+
+    let mut engine_p2 = Engine::new(&input);
+
+    for _ in 0..6 {
+        engine_p2.execute_cycle(2);
+    }
+
+    let count = engine_p2.actives.iter().count();
+    println!("Part 2: {} cubes are active after six cycles", count);
 }
