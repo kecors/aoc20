51b1d791705a7b0134ae542db633b92350e8bd8c
Kenneth Collins <dev@kecors.net>
2021-01-26 20:54:09 -0500

    Solve day 22, part 2

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -3,7 +3,7 @@ extern crate pest;
 extern crate pest_derive;
 
 use pest::Parser;
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::io::{stdin, Read};
 
 #[derive(Parser)]
@@ -11,13 +11,19 @@ use std::io::{stdin, Read};
 struct DealParser;
 
 #[derive(Debug)]
-struct Engine {
+enum Winner {
+    Player1,
+    Player2,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct State {
     deck_1: VecDeque<u32>,
     deck_2: VecDeque<u32>,
 }
 
-impl Engine {
-    fn new(input: &str) -> Engine {
+impl State {
+    fn new(input: &str) -> State {
         let mut player_id = 0;
         let mut deck_1 = VecDeque::new();
         let mut deck_2 = VecDeque::new();
@@ -41,43 +47,52 @@ impl Engine {
             }
         }
 
-        Engine { deck_1, deck_2 }
+        State { deck_1, deck_2 }
     }
+}
 
-    fn play_game(&mut self) -> u32 {
-        loop {
-            if self.deck_1.is_empty() {
-                return self
-                    .deck_2
-                    .iter()
-                    .zip((1..=self.deck_2.len()).rev())
-                    .map(|(multiplicand, multiplier)| {
-                        println!("{} x {}", multiplicand, multiplier);
-                        multiplicand * multiplier as u32
-                    })
-                    .sum();
+fn play_game(mut state: State, part2_flag: bool) -> (State, Winner) {
+    let mut states = HashSet::new();
+
+    loop {
+        if state.deck_1.is_empty() {
+            return (state, Winner::Player2);
+        }
+        if state.deck_2.is_empty() {
+            return (state, Winner::Player1);
+        }
+        if !states.insert(state.clone()) {
+            return (state, Winner::Player1);
+        }
+
+        let card_1 = state.deck_1.pop_front().unwrap();
+        let card_2 = state.deck_2.pop_front().unwrap();
+
+        let (_, winner) = if part2_flag
+            && state.deck_1.len() as u32 >= card_1
+            && state.deck_2.len() as u32 >= card_2
+        {
+            let deck_1 = state.deck_1.iter().cloned().take(card_1 as usize).collect();
+            let deck_2 = state.deck_2.iter().cloned().take(card_2 as usize).collect();
+            play_game(State { deck_1, deck_2 }, true)
+        } else {
+            (
+                state.clone(),
+                if card_1 > card_2 {
+                    Winner::Player1
+                } else {
+                    Winner::Player2
+                },
+            )
+        };
+        match winner {
+            Winner::Player1 => {
+                state.deck_1.push_back(card_1);
+                state.deck_1.push_back(card_2);
             }
-            if self.deck_2.is_empty() {
-                return self
-                    .deck_1
-                    .iter()
-                    .zip((1..=self.deck_1.len()).rev())
-                    .map(|(multiplicand, multiplier)| {
-                        println!("{} x {}", multiplicand, multiplier);
-                        multiplicand * multiplier as u32
-                    })
-                    .sum();
-            }
-
-            let card_1 = self.deck_1.pop_front().unwrap();
-            let card_2 = self.deck_2.pop_front().unwrap();
-
-            if card_1 > card_2 {
-                self.deck_1.push_back(card_1);
-                self.deck_1.push_back(card_2);
-            } else {
-                self.deck_2.push_back(card_2);
-                self.deck_2.push_back(card_1);
+            Winner::Player2 => {
+                state.deck_2.push_back(card_2);
+                state.deck_2.push_back(card_1);
             }
         }
     }
@@ -87,8 +102,35 @@ fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let mut engine = Engine::new(&input);
-
-    let score = engine.play_game();
+    let state = State::new(&input);
+    let (state, winner) = play_game(state, false);
+    let deck = match winner {
+        Winner::Player1 => state.deck_1,
+        Winner::Player2 => state.deck_2,
+    };
+    let score: u32 = deck
+        .iter()
+        .zip((1..=deck.len()).rev())
+        .map(|(multiplicand, multiplier)| {
+            //println!("{} x {}", multiplicand, multiplier);
+            multiplicand * multiplier as u32
+        })
+        .sum();
     println!("Part 1: the winning player's score is {}", score);
+
+    let state = State::new(&input);
+    let (state, winner) = play_game(state, true);
+    let deck = match winner {
+        Winner::Player1 => state.deck_1,
+        Winner::Player2 => state.deck_2,
+    };
+    let score: u32 = deck
+        .iter()
+        .zip((1..=deck.len()).rev())
+        .map(|(multiplicand, multiplier)| {
+            //println!("{} x {}", multiplicand, multiplier);
+            multiplicand * multiplier as u32
+        })
+        .sum();
+    println!("Part 2: the winning player's score is {}", score);
 }
