5fc95ad285b63c85f4bf58fbf3832a3d4b6153dd
Kenneth Collins <dev@kecors.net>
2021-02-01 08:46:58 -0500

    Solve day 10, part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -5,24 +5,50 @@ fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let mut joltages: Vec<u16> = input.lines().map(|x| x.parse::<u16>().unwrap()).collect();
-    joltages.push(0);
-    let &max = joltages.iter().max().unwrap();
-    joltages.push(max + 3);
-    joltages.sort_unstable();
+    let mut p1_joltages: Vec<i16> = input.lines().map(|x| x.parse::<i16>().unwrap()).collect();
+    let &max = p1_joltages.iter().max().unwrap();
+    let mut p2_joltages = p1_joltages.clone();
 
-    let mut differences: HashMap<u16, u16> = HashMap::new();
+    // Part 1
 
-    for index in 0..joltages.len() - 1 {
-        let difference = joltages[index + 1] - joltages[index];
+    p1_joltages.push(0);
+    p1_joltages.push(max + 3);
+    p1_joltages.sort_unstable();
+
+    let mut differences: HashMap<i16, i16> = HashMap::new();
+
+    for index in 0..p1_joltages.len() - 1 {
+        let difference = p1_joltages[index + 1] - p1_joltages[index];
         let x = differences.entry(difference).or_insert(0);
         *x += 1;
     }
 
-    if let Some(diff_1) = differences.get(&1) {
-        if let Some(diff_3) = differences.get(&3) {
-            let product = *diff_1 * *diff_3;
-            println!("Part 1: the product is {}", product);
+    let diff_1 = differences.get(&1).unwrap();
+    let diff_3 = differences.get(&3).unwrap();
+    let product = diff_1 * diff_3;
+    println!("Part 1: the product is {}", product);
+
+    // Part 2
+
+    let mut hm: HashMap<i16, u64> = HashMap::new();
+    hm.insert(0, 1);
+
+    p2_joltages.sort_unstable();
+
+    for &joltage in p2_joltages.iter() {
+        let mut quantities = 0;
+        if let Some(quantity) = hm.get(&(joltage - 3)) {
+            quantities += quantity;
         }
+        if let Some(quantity) = hm.get(&(joltage - 2)) {
+            quantities += quantity;
+        }
+        if let Some(quantity) = hm.get(&(joltage - 1)) {
+            quantities += quantity;
+        }
+        hm.insert(joltage, quantities);
     }
+
+    let ways = hm.get(&max).unwrap();
+    println!("Part 2: there are {} distinct ways", ways);
 }
